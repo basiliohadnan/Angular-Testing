376d1af118810f480430015bd79d007247815158
Hadnan Basilio | BomTrato <hbasilio@bomtrato.com>
2022-03-29 11:26:41 -0300

    Entendendo a estrutura de testes

---
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { v4 as uuiv4 } from 'uuid'
+
+@Injectable()
+export class UniqueIdService {
+
+    private numberOfGeneratedIds = 0
+    private validId = /^[A-Za-z]+[\w\-\:\.]*$/
+
+    public generateUniqueIdWithPrefix(prefix: string): string {
+        if (!prefix || !this.validId.test(prefix)) throw new Error('Prefix can not be empty!')
+        const uniqueId = this.generateUniqueId()
+        this.numberOfGeneratedIds++
+        return `${prefix}-${uniqueId}`
+    }
+
+    private generateUniqueId(): string {
+        return uuiv4()
+    }
+
+    public getNumberOfGeneratedIds(): number {
+        return this.numberOfGeneratedIds
+    }
+}
